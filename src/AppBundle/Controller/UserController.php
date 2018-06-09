4fb4968389e098899ccc4449513e7e006344776b
Eslam Mahmoud <contact@eslam.me>
2018-06-09 20:27:16 +0200

    add/edit twitch id

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AppBundle\Entity\User;
 
 class UserController extends Controller
 {
@@ -13,7 +16,28 @@ class UserController extends Controller
      */
     public function profileAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('AppBundle::user/profile.html.twig');
+        $user = $this->getUser();
+        // print_r($user);
+        $twitchForm = $this->createFormBuilder($user)
+            ->add('twitchId', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Save twitch ID'))
+            ->getForm();
+
+        $twitchForm->handleRequest($request);
+
+        if ($twitchForm->isSubmitted() && $twitchForm->isValid()) {
+            // $twitchForm->getData() holds the submitted values
+            // but, the original `$user` variable has also been updated
+            // $user = $twitchForm->getData();
+
+            //save the user to the database
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('AppBundle::user/profile.html.twig', [
+            'twitchForm' => $twitchForm->createView()
+        ]);
     }
 }
