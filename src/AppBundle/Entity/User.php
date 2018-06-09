0249800e3c576e6edf4d7a9cf1edc314e4e5aa17
Eslam Mahmoud <contact@eslam.me>
2018-06-09 18:36:47 +0200

    add user entity

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * It must be protected as it is inherited from the parent class.
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        //To override the __construct() method, be sure to call parent::__construct(), as the base User class depends on this to initialize some fields.
+        parent::__construct();
+        
+    }
+}
