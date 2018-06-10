d202e7c790a66bf6d4b9555ca6c6ff5ab12d75c1
Eslam Mahmoud <contact@eslam.me>
2018-06-11 00:22:49 +0200

    update twitch API

--- src/AppBundle/Service/TwitchAPI.php
+++ src/AppBundle/Service/TwitchAPI.php
@@ -4,6 +4,7 @@ namespace AppBundle\Service;
 
 use Unirest;
 
+//TODO add logger
 class TwitchAPI
 {
     private $id;
@@ -15,10 +16,14 @@ class TwitchAPI
         $this->secret = $secret;
     }
 
-    public function getStreams($user_login)
+    /**
+     * @param String $twtichLoginName twitch user login name
+     * @return Object twtich stream objects in array of key data of object reply
+     */
+    public function getStreams($twtichLoginName)
     {
         $headers = array('Client-ID'=>$this->id);
-        $query = array('user_login'=>$user_login);
+        $query = array('user_login'=>$twtichLoginName);
                 
         $response = Unirest\Request::get('https://api.twitch.tv/helix/streams',$headers,$query);
 
@@ -49,9 +54,52 @@ class TwitchAPI
         return $response->body;
     }
 
-    public function isUserLive($user_login)
+    //TODO merge with getStreams 
+    /**
+     * @param Array $ids twitch users ids
+     * @return Object twtich stream objects in array of key data of object reply
+     */
+    public function getStreamsByIds($ids)
     {
-        $streams = $this->getStreams($user_login);
+        $headers = array('Client-ID'=>$this->id);
+        $query = array('user_id'=>$ids);
+                
+        $response = Unirest\Request::get('https://api.twitch.tv/helix/streams',$headers,$query);
+
+        //{"data":[],"pagination":{}}
+        /*
+        {
+            "data":[
+                {
+                    "id":"29035753040",
+                    "user_id":"57781936",
+                    "game_id":"30921",
+                    "community_ids":[
+
+                    ],
+                    "type":"live",
+                    "title":"World Championship - Dignitas vs. NRG (BRACKET RESET -- GRAND FINAL)",
+                    "viewer_count":156305,
+                    "started_at":"2018-06-10T12:00:04Z",
+                    "language":"en",
+                    "thumbnail_url":"https://static-cdn.jtvnw.net/previews-ttv/live_user_rocketleague-{width}x{height}.jpg"
+                }
+            ],
+            "pagination":{
+                "cursor":"eyJiIjpudWxsLCJhIjp7Ik9mZnNldCI6MX19"
+            }
+        }
+        */
+        return $response->body;
+    }
+
+    /**
+     * @param String $twtichLoginName user login name
+     * @return Boolian true or false if user is live or not
+     */
+    public function isUserLive($twtichLoginName)
+    {
+        $streams = $this->getStreams($twtichLoginName);
         foreach ($streams->data as $key => $value) {
             if ($value->type == 'live') {
                 return true;
@@ -60,19 +108,27 @@ class TwitchAPI
         return false;
     }
 
-    public function getUserByLoginName($user_login)
+    /**
+     * @param String $twtichLoginName user login name
+     * @return Object twtich user object
+     */
+    public function getUserByLoginName($twtichLoginName)
     {
         $headers = array('Client-ID'=>$this->id);
-        $query = array('login'=>$user_login);
+        $query = array('login'=>$twtichLoginName);
                 
         $response = Unirest\Request::get('https://api.twitch.tv/helix/users',$headers,$query);
 
         return $response->body;
     }
 
-    public function getIdByLoginName($user_login)
+    /**
+     * @param String $twtichLoginName user login name
+     * @return int|null twtich user id or null of not found
+     */
+    public function getIdByLoginName($twtichLoginName)
     {
-        $twitchUser = $this->getUserByLoginName($user_login);
+        $twitchUser = $this->getUserByLoginName($twtichLoginName);
         var_dump($twitchUser);
         foreach ($twitchUser->data as $key => $value) {
             if ($value->id) {
@@ -82,4 +138,34 @@ class TwitchAPI
         //TODO exception
         return null;
     }
+
+    /**
+     * @param Integer $twitchUserId twitch user id
+     * @return Boolian true or false on success of failer of subscrioption 
+     */
+    public function webhooksSubscribeUser($twitchUserId) {
+        $headers = array(
+            'Client-ID'=>$this->id,
+            // 'Content-Type' => 'application/json',
+            'Content-Type' => 'application/x-www-form-urlencoded',
+        );
+        $query = array(
+            'hub.callback'=>'http://127.0.0.1:8000/webhooks/subscribeUser',
+            'hub.mode'=>'subscribe',
+            'hub.topic'=>'https://api.twitch.tv/helix/streams?user_id='.$twitchUserId,
+            'hub.lease_seconds' => 864000 //10 days 
+        );
+    
+        $body = Unirest\Request\Body::form($query);
+        $response = Unirest\Request::post('https://api.twitch.tv/helix/webhooks/hub',$headers,$body);
+        
+        if ($response->code < 300 && $response->code >= 200) {
+            return true;
+        } else {
+            //TODO exception
+            return false;
+        }
+    }
+
+    //TODO renew subscrition passed on hub.lease_seconds
 }
